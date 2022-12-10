5133495521ce69f03b4019b4aa245b28328a436c
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-12-10 21:37:11 +0800

    feat: 🎸 拉高 Tokens

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ async function handleEvent(event) {
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
     prompt: event.message.text ,
-    max_tokens: 200,
+    max_tokens: 500,
   });
 
   // create a echoing text message
