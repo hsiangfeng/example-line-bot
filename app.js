6a667957df7879d8094391b61bb310f74b758599
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2022-12-10 21:15:16 +0800

    feat: 🎸 add to openai api

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const line = require('@line/bot-sdk');
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 
 // create LINE SDK config from env variables
 const config = {
@@ -29,14 +36,19 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 // event handler
-function handleEvent(event) {
+async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
 
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: event.message.text ,
+  });
+
   // create a echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  const echo = { type: 'text', text: completion.data.choices[0].text };
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
